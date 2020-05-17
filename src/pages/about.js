ed90b817387f461012c7a3bfe9fce6d6b1f82f8f
parthbpatel <parthtech3010@gmail.com>
2020-05-17 16:31:02 +0530

    create simple site using gatsby.

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Header from "../components/header"
+
+export default function About() {
+  return (
+    <div style={{ color: 'teal' }}>
+      <Header headerText="About Gatsby" />
+      <Header headerText="Creative" />
+      <p>React component is amazing.</p>
+    </div>
+  )
+}
